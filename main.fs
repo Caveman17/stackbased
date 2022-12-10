4c04ce8c523980b83a410eff6fcfa2f628220e59
Patrick Kern <kern.patrick17@outlook.de>
2022-12-10 19:30:31 +0100

    basic static gamefield

--- main.fs
+++ main.fs
@@ -1,3 +1,26 @@
+: clear-screen page ;
+: print-nl 10 emit ;
 
+: blank-gamefield ( x y -- )
+  2 -
+  swap 73 2 / - swap
+  2dup     at-xy ." >   ----------------------------------| "
+  2dup 1 + at-xy ." |-- ----------------------------------| "
+  2dup 2 + at-xy ." |-- ----------------------------------| "
+  2dup 3 + at-xy ." |--                                   F "
+       4 + at-xy ." |-------------------------------------|"
+  ;
 
-1  2 3
+: banner-screen ( -- )
+  clear-screen
+
+  form ( height width )
+  2 / swap 2 /
+  blank-gamefield
+  print-nl print-nl print-nl
+  ."     "
+  ;
+
+clear-screen
+banner-screen
+
