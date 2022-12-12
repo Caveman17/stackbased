8a0773f212538291e2f63732c331c58e1c939120
Fabian Baumgartner <e11814568@student.tuwien.ac.at>
2022-12-12 19:59:08 +0100

    add basic render functions

--- main.fs
+++ main.fs
@@ -1,15 +1,26 @@
+\ Maze definition
+: line1 s" |------------------------------|" ;
+: line2 s" >   |--------------------------|" ;
+: line3 s" |-| |---|        |-------------|" ;
+: line4 s" |-|       |-----|       |------|" ;
+: line5 s" |-| |-----------| |---|        F" ;
+: line6 s" |-|               |------------|" ;
+: line7 s" |------------------------------|" ;
+
 : clear-screen page ;
 : newline 10 emit ;
 
-: blank-gamefield ( x y -- )
-  2 -
-  swap 73 2 / - swap
-  2dup     at-xy ." >   ----------------------------------| "
-  2dup 1 + at-xy ." |-- ----------------------------------| "
-  2dup 2 + at-xy ." |-- ----------------------------------| "
-  2dup 3 + at-xy ." |--                                   F "
-       4 + at-xy ." |-------------------------------------|"
-  ;
+: renderLine ( x -- )
+  type newline ;
+
+: renderLineWithPlayer ( position line len -- )
+  { position line len }
+  line position 1 - type
+    ." P"
+  line position + len position - type newline ;
+
+: swap ( x1 x2 -- x2 x1 x2 )
+  { a b } b a b .s ;
 
 : banner-screen ( -- )
   clear-screen
@@ -18,6 +29,7 @@
   ." Press s + Enter to start"
   ;
 
+
 : check-keypress ( x -- )
   begin
       key
@@ -25,14 +37,27 @@
     until
   drop ;
 
+variable xPos
+variable yPos
+10 xPos !
+1 yPos !
+
 clear-screen
 banner-screen
 115 check-keypress
 ." success"
 newline
 
-
+line1 renderLine
+line2 renderLine
+line3 renderLine
+line4 renderLine
+line5 renderLine 
+xPos @ line6
+renderLineWithPlayer
+line7 renderLine
       
+
 
 
 
